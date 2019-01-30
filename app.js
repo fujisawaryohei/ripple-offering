3afd7398c2717ba39a090f73589307183b1de3bf
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2019-01-30 18:13:28 +0900

    rippleNetwork Websocket API ws moduleからwebsocket moduleに変更

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let express = require('express');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let path = require('path');
-const Websocket = require('ws')
+// const Websocket = require('ws')
+const WebsocketClient = require('websocket').w3cwebsocket;
 let app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -23,59 +24,51 @@ app.use(function(req, res, next) {
 
 const transactionParser = require('ripple-lib-transactionparser');
 const parseBalanceChanges = transactionParser.parseBalanceChanges
-
 const servers = [
   'wss://s1.ripple.com',
   'wss://s2.ripple.com'
 ]
-const address ='';//your wallet address
+const address ='rwuwx3gWe4MC9aLgMm74hFQkJLc5fEz672';//your wallet address
+const ISSUER ='rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B';//holding your IOU's issure
+const wsc = new WebsocketClient('wss://s1.ripple.com:443');
+const requestFormat = {
+  id: 1,
+  command: "account_info",
+  account: "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59"
+}
 
-const ISSUER ='';//holding your IOU's issure
-
-let ws = new Websocket(servers[0] + ':443')
-  const subscribe = () => {
-  let ws = new Websocket(servers[0] + ':443')
-    ws.on('open',() => {
-      console.log('connected web socket')
-      ws.send(JSON.stringify({
-        id:1,
-        command:"subscribe",
-        accounts:address,
-        streams: [
-          "transactions"
-      ]
-      }))
-    })
-    ws.on('message',(message)=>{
-      const data=JSON.parse(message)
-      if (data.type === "transaction"){
-        const balanceChanges = parseBalanceChanges(data.meta)
-        transactionlog(balanceChanges,data.transaction)
-      }
-    })
-    ws.on('error',(e)=>{
-      console.log(e)
-      ws.close();
-    })
-    ws.on('close',(e)=>{
-      console.log('reconnect web socket',e.code,e.reason)
-      ws.close()
-      subscribe(ws)
-    })
+const subscribe = (wsc) => {
+    wsc.onerror = (error)=>{
+      console.log(error);
+      wsc.close();
+    }
+    wsc.onopen = ()=>{
+      console.log('websocket server connected');
+      wsc.send(JSON.stringify(requestFormat))
+    }
+    wsc.onmessage = (msg)=>{
+     const data = JSON.parse(msg.data);
+     console.log(data);
+      //  const balanceChanges = parserBalanceChanges(data)
+      //  transactionlog(balanceChanges,data,transaction);
+    }
+    wsc.onclose = (e)=>{
+      console.log('reconnect web socket',e.code,e.reason);
+      wsc.close();
+    }
  }
-
 const transactionlog = (balanceChanges,transaction)=>{
   console.log(balanceChanges)
   console.log(transaction)
 }
+subscribe(wsc);
 
-subscribe(ws)
 
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use(function(err, req, res, next) {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
