ac24a7a941a53f5199e8c3e3172ff916e78f269e
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2019-01-30 20:06:28 +0900

    add comment out

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//reference → https://developers.ripple.com/server_info.html
 let createError = require('http-errors');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
@@ -12,14 +13,15 @@ const ISSUER = config.env.issuer;//holding your IOU's issure
 const wsc = new WebsocketClient('wss://s1.ripple.com:443');
 //requestFormat refelence→https://developers.ripple.com/websocket-api-tool.html
 const requestFormat = {
-  id: 4,
-  command: "book_offers",
-  taker: "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59",
+//sample JSON
+   id: 4,
+   command: "book_offers",
+   taker: "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59",
   taker_gets: {
     currency: "XRP"
   },
-  taker_pays: {
-    fcurrency: "USD",
+  "taker_pays": {
+    currency: "USD",
     issuer: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
   },
   limit: 10
@@ -29,6 +31,7 @@ const subscribe = (wsc) => {
     wsc.onerror = (error)=>{
       console.log(error);
       wsc.close();
+      subscribe(wsc);
     }
     wsc.onopen = ()=>{
       console.log('websocket server connected');
@@ -36,13 +39,14 @@ const subscribe = (wsc) => {
     }
     wsc.onmessage = (msg)=>{
      const data = JSON.parse(msg.data);
-     console.log(data);
+     console.log(data.resultoffers);
       //  const balanceChanges = parserBalanceChanges(data)
       //  transactionlog(balanceChanges,data,transaction);
     }
     wsc.onclose = (e)=>{
       console.log('reconnect web socket',e.code,e.reason);
       wsc.close();
+      subscribe(wsc);
     }
 }
 
